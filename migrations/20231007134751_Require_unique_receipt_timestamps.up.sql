cfc8d907ae7a93971df73543ef4c53834d7b2aa3
Pablo1785 <pablo1785@wp.pl>
2023-10-07 16:19:30 +0200

    Ensure that prices contain valid keys during transaction

--- migrations/20231007134751_Require_unique_receipt_timestamps.up.sql
+++ migrations/20231007134751_Require_unique_receipt_timestamps.up.sql
@@ -1,20 +1,31 @@
 -- Add up migration script here
 BEGIN;
 
-CREATE TEMPORARY TABLE tmp AS 
+CREATE TEMPORARY TABLE tmp_receipts AS 
 SELECT receipts.id, receipts.merchant_name, receipts.paid_at FROM (
     SELECT MIN(id) AS id, paid_at FROM receipts 
     GROUP BY paid_at
 ) lft
 INNER JOIN receipts ON receipts.id = lft.id;
 
+
+CREATE TEMPORARY TABLE tmp_prices AS 
+SELECT prices.* FROM prices
+INNER JOIN tmp_receipts ON prices.receipt_id = tmp_receipts.id;
+
+DELETE FROM prices;
+
 DELETE FROM receipts;
 
 ALTER TABLE receipts 
 ADD CONSTRAINT unique_paid_at UNIQUE (paid_at);
 
-INSERT INTO receipts (id, merchant_name, paid_at) SELECT tmp.id, tmp.merchant_name, tmp.paid_at FROM tmp;
+INSERT INTO receipts (id, merchant_name, paid_at) 
+SELECT tmp_receipts.id, tmp_receipts.merchant_name, tmp_receipts.paid_at FROM tmp_receipts;
 
-DROP TABLE tmp;
+INSERT INTO prices (receipt_id, product_id, count, unit_price) 
+SELECT tmp_prices.receipt_id, tmp_prices.product_id, tmp_prices.count, tmp_prices.unit_price FROM tmp_prices;
+
+DROP TABLE tmp_receipts;
 
 COMMIT;
